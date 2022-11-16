81b00835fb8aea92fb978124c309136bd50256f0
rrachelr <rachrobins1@gmail.com>
2022-11-15 23:15:04 -0500

    Made hashNode, worked on makeHash

--- makeHash.cpp
+++ makeHash.cpp
@@ -2,5 +2,23 @@
 #include "hashMap.hpp"
 #include <iostream>
 #include <string>
+#include <fstream>
 
 using namespace std;
+
+makeHash::makeHash(string inFile, string outFile){
+    hashTable = new hashMap();
+    inputFile = inFile;
+    outputFile = outFile;
+    readFile();
+    writeFile();
+}
+
+void makeHash::readFile(){
+    ifstream inFile(inputFile);
+    string key = "";
+    string value = "";
+    inFile >> key;
+
+    
+}
--- makeHash.hpp
+++ makeHash.hpp
@@ -4,6 +4,7 @@
 using namespace std;
 
 class makeHash {
+    hashMap *hashTable;
     string inputFile;
     string outputFile;
 public:
